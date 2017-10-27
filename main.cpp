12fa66f153fd1d4c330bc93be9fe0ca792610638
Link-God <32190920+Link-God@users.noreply.github.com>
2017-10-27 14:18:24 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,146 +5,137 @@
 
 using namespace std;
 
-bool raz(int &m , int &n)
+bool read_numb_elements(unsigned int &str, unsigned int &col)
 {
 	string razmer;
-	char op; int*t = new int;
+	char op; bool sucsses = true;
+	unsigned int str_t, col_t;
 	getline(cin, razmer);
 	istringstream stream(razmer);
-	stream >> *t;
-	if (stream >> op && op == ',')
-	{
-		m =*t;
-		delete t;
-		if (!(stream >> n))
-		{
-			cout << "fail!!!";
-			return false;
-		}
-		else return true;
+	if (stream >> str_t && stream >> op && op == ',' && stream >> col_t) {
+		str = str_t;
+		col = col_t;
+
+		return true;;
 	}
-	else 
-	{
-		cout << "fail!!";
-		return false;
-	}
+	
+	return false;
 }
 
-bool read(int **matrix , int str , int col )
+bool read(double **matrix, unsigned int str , unsigned  int col )
 {
-	int k =0;
-	for (int i=0; i < str ; i++)
+	int read_matrix_numb = 0;
+	for (unsigned int i = 0; i < str; i++)
 	{
 		string row;
 		getline(cin, row);
 		istringstream stream(row);
-		for (int j=0; j < col; j++)
+		for (unsigned int j = 0; j < col; j++)
 		{
-			if (stream >> matrix[i][j]) { k++; }
+			if (stream >> matrix[i][j]) { read_matrix_numb++; }
 		}
 	}
-	if (k == str*col) return true;
+	if (read_matrix_numb == str*col) return true;
 	else return false;
 }
 
-void print(int **matrix , int str , int col)
+void print(double **matrix  , unsigned int str , unsigned int col)
 {
-	for (int i = 0; i < str; i++)
+	for (unsigned int i = 0; i < str; i++)
 	{
 		cout << endl;
-		for (int j = 0; j < col; j++)
+		for (unsigned int j = 0; j < col; j++)
 		{
-			cout << matrix[i][j]<< " ";
+			if (matrix[i][j] == -0.0) { cout << 0 << " "; }
+			else { cout << matrix[i][j] << " " ; }
 		}
 	}
 }
-
-void add(int ** mat1, int ** mat2,int str,int col)
+void del(double ** matrix , unsigned int str)
 {
-	for (int i = 0; i < str; i++)
+	for (unsigned int i = 0; i < str; i++)
 	{
-		cout << endl;
-		for (int j = 0; j < col; j++)
+		delete[] matrix[i];
+	}
+	delete matrix;
+}
+double** creat(unsigned int str , unsigned int col)
+{
+	double ** mat3 = new double*[str];
+	for (unsigned int i = 0; i < str; i++)
+	{
+		mat3[i] = new double[col];
+	}
+	return mat3;
+}
+void add(double ** &mat3, double ** mat1, double ** mat2, unsigned int str, unsigned int col)
+{
+	mat3 = new double *[str];
+
+	for (unsigned int i = 0; i < str; i++)
+	{
+		mat3[i] = new double[col];
+		for (unsigned int j = 0; j < col; j++)
 		{
-			cout << mat1[i][j] + mat2[i][j] << " ";
+			mat3[i][j] = mat1[i][j] + mat2[i][j] ;
+		}
+	}
+} 
+void sub(double ** &mat3, double ** mat1, double ** mat2, unsigned int str, unsigned int col)
+{
+	mat3 = new double *[str];
+	for (unsigned int i = 0; i < str; i++)
+	{
+		mat3[i] = new double[col];
+		for (unsigned int j = 0; j < col; j++)
+		{
+			mat3[i][j] = mat1[i][j] - mat2[i][j] ;
 		}
 	}
 }
-
-void sub(int ** mat1, int ** mat2, int str, int col)
+void mul(double ** mat1, double ** mat2, double ** & mat3, unsigned int str, unsigned int col, unsigned int col1)
 {
-	for (int i = 0; i < str; i++)
+	mat3 = new double *[str];
+	for (unsigned int i = 0; i < str; i++)
 	{
-		cout << endl;
-		for (int j = 0; j < col; j++)
+		mat3[i] = new double[col];
+		for (unsigned int j = 0; j < col; j++)
 		{
-			cout << mat1[i][j] - mat2[i][j] << " ";
-		}
-	}
-}
-
-void mul(int ** mat1, int ** mat2, int str, int col,int col1)
-{
-	int ** mat3 = new int*[str];
-	for (int i = 0; i < str; i++)
-	{
-		mat3[i] = new int[col];
-	}
-	for (int i = 0; i < str; i++) {
-		cout << endl;
-		for (int j = 0; j < col; j++) {
-			int res = 0;
-			for (int k = 0; k < col1; k++) {
+			double res = 0;
+			for (unsigned int k = 0; k < col1; k++)
+			{
 				res += mat1[i][k] * mat2[k][j];
 			}
-
 			mat3[i][j] = res;
-			cout << mat3[i][j]<<" ";
 		}
 	}
 }
 
-void T(int ** mat3 , int **matrix1 ,int col1 , int str1)
+void T(double ** & mat3 , double **matrix1 , unsigned int col1 , unsigned int str1)
 {
-	for (int i = 0; i < col1; i++)
-	{
-		mat3[i] = new int[str1];
-	}
-	for (int i = 0; i < col1; ++i)
-	{
-		for (int j = 0; j < str1; ++j)
-		{
-			mat3[i][j] = matrix1[j][i];
-		}
-	}
-}
-
-void TR(double ** mat3, double **matrix1, int col1, int str1)
-{
-	for (int i = 0; i < col1; i++)
+	mat3 = new double *[col1];
+	for (unsigned int i = 0; i < col1; ++i)
 	{
 		mat3[i] = new double[str1];
-	}
-	for (int i = 0; i < col1; ++i)
-	{
-		for (int j = 0; j < str1; ++j)
+		for (unsigned int j = 0; j < str1; ++j)
 		{
 			mat3[i][j] = matrix1[j][i];
 		}
 	}
 }
 
-void min_matrix(int ** mat, int **n_mat, int i,int j, int N)
+
+void min_matrix(double ** mat, double **n_mat, unsigned int i,unsigned int j, unsigned int N)
 {
-	int k = 0; int  g;
-	for (int i1 = 0;  i1<N -1; i1++) 
+	unsigned int k = 0; unsigned int  g;
+	for (unsigned int i1 = 0;  i1<N -1; i1++) 
 	{ 		
 		if (i1 == i)
 		{
 			k = 1;
 		}
 			g = 0;
-		for (int j1 = 0; j1< N - 1; j1++) 
+		for (unsigned int j1 = 0; j1< N - 1; j1++) 
 		{
 			if (j1 == j)
 			{
@@ -155,27 +146,24 @@ void min_matrix(int ** mat, int **n_mat, int i,int j, int N)
 	}
 }
 
-int det(int ** mat, int N)
+double det(double ** mat, unsigned int N)
 {
-	int i=0;
-	int deter=0;
-	int M = N - 1;
-	int step = 1;
+	unsigned int i=0;
+	double deter=0;
+	unsigned int M = N - 1;
+	char step = 1;
 
-	int ** n_mat;
-	n_mat = new int *[N];
-	for (int i = 0; i < N; i++)
+	double ** n_mat;
+	n_mat = new double *[N];
+	for (unsigned int i = 0; i < N; i++)
 	{
-		n_mat[i] = new int[N];
+		n_mat[i] = new double[N];
 	}
 
 	if (N < 1)
 	{
-		for (int i = 0; i < N; i++)
-		{
-			delete[] n_mat[i];
-		}
-		cout << "fail";
+		del(n_mat, N);
+		cout << "fail(1x1 matrix ?)";
 	}
 	if (N == 1)
 	{
@@ -186,10 +174,7 @@ int det(int ** mat, int N)
 	if (N == 2)
 	{
 		deter = mat[0][0] * mat[1][1] - (mat[1][0] * mat[0][1]);
-		for (int i = 0; i < N; i++)
-		{
-			delete[] n_mat[i];
-		}
+		del(n_mat, N);
 		return(deter);
 	}
 	if (N>2)
@@ -201,80 +186,54 @@ int det(int ** mat, int N)
 			step = -step;
 		}
 	}
-	for (int i = 0; i < N; i++)
-	{
-		delete[] n_mat[i];
-	}
+	del(n_mat,N);
 	return(deter);
 }
 
-void R(double ** matR, int ** matrix1, int str1, int col1)
+void R(double ** & mat3, double ** matrix1, unsigned int str1, unsigned int col1,bool &print )
 {
-	int **n_mat;
-	double ** mat;
-	n_mat = new int*[str1];
-	mat = new double*[str1];
-	for (int i = 0; i < str1; i++)
-	{
-		matR[i] = new double[col1];
-		n_mat[i] = new int[col1];
-	}
-	int deter = det(matrix1, str1);
+	mat3 = new double *[str1];
+	auto matR = creat(str1, col1);
+	double deter = det(matrix1, str1);
 	if (deter != 0)
 	{
-		for (int i = 0; i < str1; i++)
+		auto n_mat = creat(str1, col1);
+		for (unsigned int i = 0; i < str1; i++)
 		{
-			for (int j = 0; j < col1; j++)
+			mat3[i] = new double[col1];
+			for (unsigned int j = 0; j < col1; j++)
 			{
+				
 				min_matrix(matrix1, n_mat, i, j, str1);
 				double d = det(n_mat, (str1 - 1));
 				matR[i][j] = d * pow((-1), (i + j)) * (1.0 / deter);
 			}
 		}
-		TR(mat, matR, str1, col1);
-		for (int i = 0; i < col1; ++i)
-		{
-			cout << endl;
-			for (int j = 0; j < str1; ++j)
-			{
-				cout << mat[i][j] << " ";
-			}
-
-		}
+		del(n_mat, str1);
+		T(mat3, matR, str1, col1);
+		del(matR, str1);
 	}
-	else cout << "fail R , det = 0";
-	if (deter != 0)
+	else 
 	{
-		for (int i = 0; i < str1; i++)
-		{
-			delete[] n_mat[i];
-		}
-		for (int i = 0; i < str1; i++)
-		{
-			delete[] mat[i];
-		}
+		print = false;
+		cout << "fail R , det = 0"; 
 	}
 }
 
 int main()
 {
-	int str1, col1;
-	int str2, col2;
-	static int **matrix1;
-	static bool mat1=false ,mat2=false;
-	static int **matrix2;
-	static char op;
-	if (raz(str1, col1))
+	unsigned int str1, col1;
+	unsigned int str2, col2;
+	double **matrix1 = nullptr;
+	bool mat1=false ,mat2=false;
+	double **matrix2 = nullptr;
+	char op='n';
+	if (read_numb_elements(str1, col1))
 	{
-		matrix1 = new int*[str1];
-		for (int i = 0; i < str1; i++)
-		{
-			matrix1[i] = new int[col1];
-		}
+		matrix1 = creat(str1, col1);
 		if (read(matrix1, str1, col1))
 		{
 			mat1 = true;
-			//cout << "op  " << endl ;
 			cin >> op;
 			cin.get();
 		}
@@ -282,13 +241,9 @@ int main()
 	}
 	if (op == '+' || op == '-' || op == '*')
 	{
-		if (raz(str2, col2))
+		if (read_numb_elements(str2, col2))
 		{
-			matrix2 = new int*[str2];
-			for (int i = 0; i < str2; i++)
-			{
-				matrix2[i] = new int[col2];
-			}
+			matrix2 = creat(str2, col2);
 			if (read(matrix2, str2, col2))
 			{
 				mat2 = true;
@@ -304,7 +259,10 @@ int main()
 		case '+':
 			if (str1 == str2 && col1 == col2)
 			{
-				add(matrix1, matrix2, str1, col1);
+				double ** mat3;
+				add(mat3 ,matrix1, matrix2, str1, col1);
+				print(mat3, str1, col1);
+				del(mat3, str1);
 			}
 			else
 			{
@@ -315,7 +273,10 @@ int main()
 		case '-':
 			if (str1 == str2 && col1 == col2)
 			{
-				sub(matrix1, matrix2, str1, col1);
+				double ** mat3;
+				sub(mat3 ,matrix1, matrix2, str1, col1);
+				print(mat3, str1, col1);
+				del(mat3, str1);
 			}
 			else
 			{
@@ -326,7 +287,9 @@ int main()
 		case '*':
 			if (col1 == str2)
 			{
-				mul(matrix1, matrix2, str1, col2,col1);
+				double ** mat3;
+				mul(matrix1, matrix2,mat3, str1, col2,col1);
+				print(mat3, str1, col2);
 			}
 			else
 			{
@@ -339,40 +302,31 @@ int main()
 	}
 	else if (op == 'T')
 	{
-		int ** mat3;
-		 mat3 = new int*[col1];
-		 T(mat3, matrix1, col1, str1);
+		double ** mat3;
+		T(mat3, matrix1, col1, str1);
 		print(mat3, col1, str1);
-		for (int i = 0; i < col1; i++)
-		{
-			delete[] mat3[i];
-		}
+		del(mat3, col1);
 
 	}
 	else if (op == 'R')
 	{
-		double ** matR; 
-		matR = new double*[str1];
-		R(matR, matrix1, str1, col1);
-		for (int i = 0; i < str1; i++)
+		bool print_t= true;
+		double ** mat3;
+		R(mat3,  matrix1, str1, col1,print_t);
+		if (print_t) 
 		{
-			delete[] matR[i];
+			print(mat3, str1, col1);
+			del(mat3, str1);
 		}
 	}
 	cin.get();
 	if (mat1)
 	{
-		for (int i = 0; i < str1; i++)
-		{
-			delete[] matrix1[i];
-		}
+		del(matrix1, str1);
 	}
 	if (mat2)
 	{
-		for (int i = 0; i < str2; i++)
-		{
-			delete[] matrix2[i];
-		}
+		del(matrix2, str2);
 	}
 	return 0;
 }
